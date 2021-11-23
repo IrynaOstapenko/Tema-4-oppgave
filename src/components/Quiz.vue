24c99a7a2525a6946bcb8e1f5a3cdc151d19854c
Julie Viktoria <julieviktoria1@hotmail.com>
2021-11-23 13:42:12 +0100

    🐛 fixed localhost

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,24 +1,5 @@
-<<<<<<< HEAD
-<!-- <template>
-  <section class="quizContainer">
-    <div class="quizContainer__Question">
-      <h2>{{ question }}</h2>
-    </div>
-    <div class="quizContainer__Answers">
-      <button class="quizContainer__Answers--buttons">Answer 1</button>
-      <button class="quizContainer__Answers--buttons">Answer 2</button>
-      <button class="quizContainer__Answers--buttons">Answer 3</button>
-      <button class="quizContainer__Answers--buttons">Answer 4</button>
-    </div>
-  </section>
-
-  <audio controls autoplay hidden>
-    <source src="./../../assets/music-kahoot.mp3" type="audio/mpeg" />
-  </audio>
-=======
 <template>
 	<h3>Quiz</h3>
->>>>>>> d11d17a860975dc7cf123e21d69a580086ef89f4
 </template>
 
 <script>
@@ -56,59 +37,7 @@ export default {
 
 
 <style>
-<<<<<<< HEAD
-body {
-  background: rgb(41, 41, 41);
-  font-family: Arial, Helvetica, sans-serif;
-}
-.quizContainer {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  width: 100%;
-  height: 100%;
-}
-.quizContainer__Question {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 20%;
-  color: white;
-}
-.quizContainer__Answers {
-  display: grid;
-  height: 25%;
-  width: 33%;
-  grid-template: 1fr 1fr/ 1fr 1fr;
-  gap: 1rem;
-}
-.quizContainer__Answers--buttons {
-  height: 100%;
-  width: 100%;
-  border: none;
-  cursor: pointer;
-  font-weight: bold;
-}
-.quizContainer__Answers--buttons:hover {
-  opacity: 0.8;
-}
-.quizContainer__Answers--buttons:nth-child(1) {
-  background: crimson;
-}
-.quizContainer__Answers--buttons:nth-child(2) {
-  background: yellow;
-}
-.quizContainer__Answers--buttons:nth-child(3) {
-  background: lightseagreen;
-}
-.quizContainer__Answers--buttons:nth-child(4) {
-  background: rgb(82, 189, 255);
-}
-</style> -->
-=======
     h3 {
 		text-align: center;
 	}
 </style>
->>>>>>> d11d17a860975dc7cf123e21d69a580086ef89f4
