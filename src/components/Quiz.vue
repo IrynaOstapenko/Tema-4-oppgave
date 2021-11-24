81b48f4e6fede59634fa4077ed2f106b5b96d631
Julie Viktoria <julieviktoria1@hotmail.com>
2021-11-24 08:41:39 +0100

    🔥 remove code

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,43 +1,13 @@
 <template>
-	<h3>Quiz</h3>
+  
 </template>
 
 <script>
-const questions = [
-  {
-    question: "How many days are there in a year?",
-    answers: [
-      { answer: "324", wrong: true },
-      { answer: "311", wrong: true },
-      { answer: "365", wrong: false },
-      { answer: "355.3", wrong: true },
-    ],
-  },
-  {
-    question: "How many continents exists on Earth?",
-    answers: [
-      { answer: "7", wrong: false },
-      { answer: "4", wrong: true },
-      { answer: "6", wrong: true },
-      { answer: "3", wrong: true },
-    ],
-  },
-];
-console.log(questions);
-const giveQuestion = function () {};
-giveQuestion();
 export default {
-  data() {
-    return {
-      question: (this.question = questions[0].question),
-    };
-  },
-};
+ 
+}
 </script>
 
-
 <style>
-    h3 {
-		text-align: center;
-	}
+
 </style>
