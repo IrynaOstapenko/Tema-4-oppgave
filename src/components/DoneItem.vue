5ab574809f869b1449323aed3a9bafa3be77ca1b
Iryna Ostapenko <irinaostap@yahoo.com>
2021-12-04 15:29:51 +0100

    Added todo

---
+++ src/components/DoneItem.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="done-item">
+		<div class="done-item__task">{{ doneTask }}</div>
+        <button class="done-item__delete-button" @click="emitRemoveFromDone">
+            <DeleteIcon />
+        </button>
+	</div>
+</template>
+
+<script>
+    import DeleteIcon from './DeleteIcon.vue'
+
+	export default {
+		components: {
+            DeleteIcon
+		},
+		props: {
+			doneTask: { type: String }
+		},
+
+		methods: {
+			emitRemoveFromDone() {
+				this.$emit('remove-from-done', this.doneTask)
+			}   
+		}
+	}
+</script>
+
+<style>
+    .done-item {
+	   width: 98%;
+       margin-left: auto;
+       margin-right: auto;
+       border-radius: 5px;
+       display: flex;
+       flex-flow: row nowrap;
+       justify-content: space-between;
+	   padding-left: 0.5em;
+	   padding-right: 0.5em;
+       align-items: center;
+       align-content: center;  
+       background-color: #ffff;
+	   text-decoration: line-through;
+   }
+
+   .done-item__task {
+      width: 77%;
+       height: 30px;
+       margin-left: 10px;
+       padding-top: 5px;
+       background-color: #ffff; 
+       border-radius: 5px; 
+   }
+
+   .done-item__delete-button {
+       height: 15px;
+       width: 15px;
+       margin-right: 7px;
+       border: none;
+       background-color: white;
+   }
+
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,84 @@
+<template>
+	<div class="todo-item">
+		<div class="todo-item__task">{{ task }}</div>
+		<button class="todo-item__done-button" @click="emitDone">
+			<DoneIcon />
+		</button>
+		
+		<button class="todo-item__delete-button" @click="emitRemove">
+			<DeleteIcon />
+		</button>
+	</div>
+</template>
+
+<script>
+	import DoneIcon from './DoneIcon.vue'
+    import DeleteIcon from './DeleteIcon.vue'
+
+	export default {
+		components: {
+			DoneIcon,
+            DeleteIcon
+		},
+		props: {
+			task: { type: String }
+		},
+
+		methods: {
+			emitDone() {
+				this.$emit('move-to-done', this.task)
+			},
+			emitRemove() {
+				this.$emit('remove-task', this.task)
+			}   
+		}
+	}
+</script>
+
+<style>
+   .todo-item {
+       width: 98%;
+       margin-left: auto;
+       margin-right: auto;
+	   margin-bottom: 10px;
+       border-radius: 5px;
+       display: flex;
+       flex-flow: row nowrap;
+       justify-content: space-around;
+       align-items: center;
+       align-content: center;  
+       background-color: #ffff; 
+   }
+
+   .todo-item__task {
+       width: 77%;
+       height: 30px;
+       margin-left: 10px;
+       padding-top: 5px;
+       background-color: #ffff; 
+       border-radius: 5px;
+   }
+
+   .todo-item__done-button {
+       height: 20px;
+       width: 20px;
+       background-color: #ffff;
+       border: none;
+   }
+ 
+   .todo-item__delete-button {
+       height: 15px;
+       width: 15px;
+       background-color: #ffff;
+       border: none;
+       border-radius: 2px;
+       cursor: pointer;
+   }
+
+   .todo-item__delete-button:hover {
+       background-color: white;
+   }	
+   
+</style>
+
+
