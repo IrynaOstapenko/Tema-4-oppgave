43125462e4aa23c92d85e95e9fe23ff01145480b
Julie Viktoria <julieviktoria1@hotmail.com>
2021-12-07 09:38:38 +0100

    🎨 new content, update style

--- src/components/SortableTable.vue
+++ src/components/SortableTable.vue
@@ -3,9 +3,8 @@
 		<thead>
 			<tr>
 				<td>Name</td>
-				<td>Year</td>
-				<td>Color</td>
-				<td>Country</td>
+				<td>Email</td>
+				<td>Department</td>
 			</tr>
 		</thead>
 
@@ -18,7 +17,13 @@
 </template>
 
 <script>
-	const mockData = `[{ "name": "Venture", "year": 1997, "color": "Khaki", "country": "Argentina" }, { "name": "Xterra", "year": 2006, "color": "Puce", "country": "Czech Republic" }, { "name": "Voyager", "year": 1992, "color": "Green", "country": "Guatemala" }, { "name": "C-MAX Hybrid", "year": 2013, "color": "Maroon", "country": "Russia" }, { "name": "Sable", "year": 1986, "color": "Maroon", "country": "Poland" }, { "name": "DTS", "year": 2011, "color": "Red", "country": "China" }, { "name": "E-Class", "year": 2001, "color": "Aquamarine", "country": "China" }, { "name": "Talon", "year": 1998, "color": "Green", "country": "Indonesia" }, { "name": "G", "year": 2004, "color": "Pink", "country": "China" }, { "name": "Tracker", "year": 1994, "color": "Crimson", "country": "Russia" }, { "name": "SC", "year": 2000, "color": "Fuscia", "country": "Portugal" }, { "name": "LS", "year": 2010, "color": "Khaki", "country": "China" }, { "name": "3 Series", "year": 1997, "color": "Indigo", "country": "Philippines" }, { "name": "Town Car", "year": 2006, "color": "Pink", "country": "China" }, { "name": "TT", "year": 2008, "color": "Indigo", "country": "China" }, { "name": "Sidekick", "year": 1992, "color": "Fuscia", "country": "Guinea" }, { "name": "I", "year": 1998, "color": "Purple", "country": "Portugal" }, { "name": "Impala", "year": 2002, "color": "Puce", "country": "Indonesia" }, { "name": "B-Series", "year": 2007, "color": "Yellow", "country": "Croatia" }, { "name": "L-Series", "year": 2000, "color": "Crimson", "country": "France" }]`;
+	// random data from Mockaroo with some detail changes
+	const mockData = `[	{"name":"Debora Robelet","email":"drobelet0@theme.com","department":"Sales"},
+						{"name":"Bren Greenfield","email":"bgreenfield1@theme.com","department":"Legal"},
+						{"name":"Irv Leupoldt","email":"ileupoldt2@theme.com","department":"Accounting"},
+						{"name":"Patten Clipsham","email":"pclipsham3@theme.org","department":"Services"},
+						{"name":"Lauraine Puve","email":"lpuve4@theme.com","department":"Product Managment"},
+						{"name":"Ripley Cockayne","email":"rcockayne5@theme.edu","department":"CEO"}]`;
 
 	export default {
 		data() {
@@ -46,17 +51,24 @@
 <style>
 	table {
 		width: 50vw;
-		border-collapse: collapse;
+		border: solid black 1px;
 		margin: auto;
+		padding: 15px;
+		border-radius: 25px;
 	}
 
 	table thead {
-		font-weight: bold;
+		font-weight: 400;
+		font-family: 'Quicksand', sans-serif;
+		font-size: 1.5em;
 	}
 
 	table tr {
-		border: 1px solid black;
+		font-family: 'Quicksand', sans-serif;
+		font-size: 0.8em;
 	}
+
+	tr:nth-child(even) {background-color: #f1eaea;}
 
 	table td {
 		padding: 0.4em;
